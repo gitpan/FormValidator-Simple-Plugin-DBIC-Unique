0294f10a6b41fe42193dd284bd9dbd95e4d9b99e
Lyo Kato <lyokato@cpan.org>
2005-11-23 16:04:08 +0000

    Import of LYOKATO/FormValidator-Simple-Plugin-DBIC-Unique-0.03 from CPAN.
    gitpan-cpan-distribution: FormValidator-Simple-Plugin-DBIC-Unique
    gitpan-cpan-version:      0.03
    gitpan-cpan-path:         LYOKATO/FormValidator-Simple-Plugin-DBIC-Unique-0.03.tar.gz
    gitpan-cpan-author:       LYOKATO
    gitpan-cpan-maturity:     released

--- lib/FormValidator/Simple/Plugin/DBIC/Unique.pm
+++ lib/FormValidator/Simple/Plugin/DBIC/Unique.pm
@@ -6,7 +6,7 @@ use UNIVERSAL::require;
 use FormValidator::Simple::Exception;
 use FormValidator::Simple::Constants;
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 sub DBIC_UNIQUE {
 
@@ -88,8 +88,8 @@ FormValidator::Simple::Plugin::DBIC::Unique - unique check for DBIC
     ] );
 
     # you can set cdbi_base_class in option.
-    my $valid = FormValidator::Simple->new( { cdbi_base_class => 'MyProj::Model' } );
-    $valid->check( $q => [
+    FormValidator::Simple->set_option( dbic_base_class => 'MyProj::Model' );
+    FormValidator::Simple->check( $q => [
         name => [ [qw/DBIC_UNIQUE User name/] ],
     ] );
 
